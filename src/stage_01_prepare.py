06e9e5919fb4c5fd4cc7f3f59a8ce5b5c54781de
damanpreet26 <damanpreets26@gmail.com>
2021-12-29 14:53:54 +0530

    train-test file created

--- src/stage_01_prepare.py
+++ src/stage_01_prepare.py
@@ -26,9 +26,22 @@ def main(config_path, params_path):
     seed_val = params["prepare"]["seed"]
     random.seed(seed_val)
     #print(seed_val)
-    print(os.path.join(config["artifacts"]["artifacts_dir"],config["artifacts"]["prepared_data_dir"]))
-    create_dir([os.path.join(config["artifacts"]["artifacts_dir"],config["artifacts"]["prepared_data_dir"])])
+    artifacts=config["artifacts"]
+    create_dir([os.path.join(artifacts["artifacts_dir"],artifacts["prepared_data_dir"])])
 
+    train_data_path=os.path.join(artifacts["artifacts_dir"],artifacts["prepared_data_dir"],artifacts["train_data"])
+    test_data_path=os.path.join(artifacts["artifacts_dir"],artifacts["prepared_data_dir"],artifacts["test_data"])
+
+    print(train_data_path)
+    print(test_data_path)
+    print(input_data)
+
+    encode="utf8"
+    with open(input_data, "r", encoding=encode) as data_file:
+        with open(train_data_path,"w", encoding=encode) as train_file:
+            with open(test_data_path,"w",encoding=encode) as test_file:
+                pass
+                #process_post(data_file, train_file, test_file,"<python>",split)
 if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config","--c",default="config/config.yaml")
